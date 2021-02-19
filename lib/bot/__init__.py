f12236875ba9db75f3f7640b25b0bdbdf4f89c2a
Gamearoo Codes <gamearoo@animetopic.pro>
2021-02-18 20:20:38 -0500

    1.0

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,11 +1,16 @@
 # modules
+from asyncio.tasks import sleep
+from glob import glob
+from ..db import db
 from discord import Intents
 from discord.ext.commands import Bot as BotBase
+from apscheduler.triggers.cron import CronTrigger
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from discord import Embed
 import yaml
 from datetime import datetime
 from pytz import timezone
+from discord.ext.commands import CommandNotFound
 
 now_utc = datetime.now(timezone('UTC'))
 
@@ -19,6 +24,20 @@ now_time = now_utc.astimezone(timezone(config['timezone']))
 # definitions
 PREFIX = config['prefix']
 OWNER_IDS = config['ownerids']
+COGS = [path.split("\\")[-1][:-3] for path in glob('./lib/cogs/*.py')]
+
+
+class Ready(object):
+    def __init__(self):
+        for cog in COGS:
+            setattr(self, cog, False)
+
+    def ready_up(self, cog):
+        setattr(self, cog, True)
+        print(f'{cog} cog Ready!')
+
+    def all_ready(self):
+        return all([getattr(self, cog) for cog in COGS])
 
 # start of bot
 
@@ -28,10 +47,13 @@ class Bot(BotBase):
         # self definitions
         self.PREFIX = PREFIX
         self.ready = False
+        self.cogs_ready = Ready()
         self.guild = None
         self.SUP = None
         self.scheduler = AsyncIOScheduler()
         self.config = config
+
+        db.autosave(self.scheduler)
 
         intents = Intents.default()
         intents.members = True
@@ -39,8 +61,17 @@ class Bot(BotBase):
         # *Start of main bot code
         super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS, intents=intents)
 
+    def setup(self):
+        for cog in COGS:
+            self.load_extension(f"lib.cogs.{cog}")
+            print(f"{cog} cog loaded")
+        print('Setup Completed!')
+
     def run(self, VERSION):
         self.VERSION = VERSION
+
+        print("Running Setup....")
+        self.setup()
 
         self.TOKEN = config['token']
 
@@ -48,16 +79,37 @@ class Bot(BotBase):
         super().run(self.TOKEN, reconnect=True)
 
     async def on_connect(self):
-        print("Bot is Ready!")
+        print("Bot connected!")
 
     async def on_disconnect(self):
         print('Bot Lost Internet Connection!')
 
+    async def on_error(self, err, *args, **kwargs):
+        if err == 'on_command_error':
+            await args[0].send("Something Went Wrong.")
+
+        raise
+
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            pass
+
+        elif hasattr(exc, "original"):
+            raise exc.original
+
+        else:
+            raise exc
+
     async def on_ready(self):
         if not self.ready:
-            self.ready = True
+
             self.SUP = self.get_guild(config['support-server'])
-            channel = self.SUP.get_channel(config['startchannel'])
+
+            self.scheduler.start()
+            while not self.cogs_ready.all_ready():
+                await sleep(0.5)
+
+            channel = self.SUP.get_channel(config['botlog'])
 
             embed = Embed(
                 title="Now Online!",
@@ -69,8 +121,9 @@ class Bot(BotBase):
                 name=f"{self.user.display_name}#{self.user.discriminator}", icon_url=f"{self.user.avatar_url}")
             embed.set_thumbnail(url=f'{self.SUP.icon_url}')
             await channel.send(embed=embed)
+            self.ready = True
 
-            print(f'Bot Ready! On Version {self.VERSION}')
+            print(f'Bot is Ready! On Version {self.VERSION}')
         else:
             print('Bot Reconnected!')
             channel = self.SUP.get_channel(config['startchannel'])
